fba4bb7bb8e35716d9106cf07549b779c9f9fef0
Abel Henry-Lapassat <98323161+Jvaljer@users.noreply.github.com>
2024-12-03 19:33:16 +0100

    finished page style

--- prep-me/src/views/FinishedPage.vue
+++ prep-me/src/views/FinishedPage.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="finished-container" class="v-container">
         <div class="header-container">
-            <span class="title">Well Done !</span>
-            <span class="subtitle">You're now ready to perform with a minimal risk of injury</span>
+            <span class="header-title snd-txt">Well Done !</span>
+            <span class="header-subtitle fst-txt">You're now ready to perform with a minimal risk of injury</span>
         </div>
         <img src="@/assets/images/done-icon.png" alt="" class="finished-img">
     </div>
@@ -15,7 +15,14 @@
   
 <style lang="less" scoped>
 #finished-container {
-    // TODO
-
+    .header-title {
+        font-size: 3rem;
+        font-weight: bold;
+        text-align: center;
+    }
+    .header-subtitle {
+        font-size: 2rem;
+        line-height: 1;
+    }
 }
 </style>
