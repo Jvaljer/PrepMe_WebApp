e483fcf646a7bb1ec01daa91e0f0a74403b80d95
Abel Henry-Lapassat <98323161+Jvaljer@users.noreply.github.com>
2025-01-04 20:04:21 +0100

    StretchPage done (handle max stretch)

--- src/views2/StretchPage2.vue
+++ src/views2/StretchPage2.vue
@@ -1,12 +1,33 @@
 <template>
-
-    <div id="stretch-container" class="v-container">
-        <h1>Stretching for {{ sport }}</h1>
+    <div id="stretch-container-2" class="v-t-container">
+        <div class="stretch2-content" :style="backgroundImage">
+            <div class="stretch2-header">
+                <span class="stretch2-title">{{ sport }} stretching</span>
+                <span class="stretch2-indication">{{ stretchId+1 }} - {{ stretchName }}</span>
+            </div>
+            <img :src=getIllustrationPath() class="stretch2-img"/>
+            <div class="stretch2-toolbar">
+                <img src="@/assets2/icons/cross.png" alt="" class="stretch2-icon" @click="closeSession()"/>
+                <img src="@/assets2/icons/question-mark.png" alt="" class="stretch2-icon" @click="toggleAdvice()"/>
+            </div>
+        </div>
+        <div class="stretch2-infos">
+            <span class="stretch2-description">{{ stretchDescription }}</span>
+            <div class="stretch2-hint">
+                <img src="@/assets2/icons/clock.png" alt="" class="stretch2-icon"/>
+                <span class="stretch2-text">{{ stretchDuration }}</span>
+            </div>
+        </div>
+        <div class="stretch2-navbar">
+            <button class="blue-btn-small" @click="previousStretch()">Previous</button>
+            <button class="yellow-btn-small" @click="nextStretch()">Next Stretch</button>
+        </div>
     </div>
 </template>
 
 
 <script setup>
+import { computed, onMounted, ref } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 
 import skateStretchesData from '@/resources/skate-stretches.json';
@@ -20,13 +41,17 @@ import gymLegsData from '@/resources/gym-legs-stretches.json';
 const router = useRouter();
 const route = useRoute();
 
-const sport = route.query.sport || 'ERR';
+const sport = route.query.sport || 'ERR_NO-SPORT';
+const sportName = sport.toLowerCase();
+const session = route.query.session || 'ERR_NO-SESSION';
 
 const stretchId = ref('ERR_STRETCH_NO-ID');
 const stretchName = ref('');
 const stretchDescription = ref('ERR_STRETCH_NO-DESCRIPTION');
 const stretchDuration = ref('ERR_STRETCH_NO-DURATION');
 const stretchAdvice = ref('ERR_STRETCH_NO-ADVICE');
+
+const images = import.meta.glob('@/assets2/images/*.png', { eager: true });
 
 const stretchAmount = ref(0);
 const stretches = ref(null);
@@ -39,6 +64,17 @@ onMounted(() => {
     stretchId.value = 0;  // Setting the stretchId to 0
     stretchAmount.value = getStretchesAmount();
     updateStretch();
+});
+
+const backgroundImage = computed(() => {
+    const imagePath = images[`/src/assets2/images/${sportName}-cover.png`];
+
+    if (imagePath) {
+        return { backgroundImage: `url(${imagePath.default})` };
+    } else {
+        console.error('Image not found:', sportName);
+        return { backgroundImage: 'none' };
+    }
 });
 
 function getStretchesData()
@@ -59,16 +95,16 @@ function getStretchesData()
 }
 function getBodyPartStretches()
 {
-    switch (bodyPart)
+    switch (session)
     {
-        case 'pull':
+        case 'Pull':
             return gymPullData;
-        case 'push':
+        case 'Push':
             return gymPushData;
-        case 'legs':
+        case 'Legs':
             return gymLegsData;
         default:
-            return 'ERR_BODYPARTNOTFOUND';
+            return 'ERR_BODY-PART-NOT-FOUND';
     }
 }
 function getStretchName(id)
@@ -107,7 +143,7 @@ function updateStretch()
 function getIllustrationPath()
 {
     // const filePath = "/illustrations/"+sportName+"/stretch-"+stretchId.value+".png";
-    const filePath = (sport==='Gym') ? `/illustrations2/${sportName}/${bodyPart}/stretch-${stretchId.value}.png` : `/illustrations/${sportName}/stretch-${stretchId.value}.png`;
+    const filePath = (sport==='Gym') ? `/illustrations2/${sportName}/${session.toLowerCase()}/stretch-${stretchId.value}.png` : `/illustrations2/${sportName}/stretch-${stretchId.value}.png`;
     return filePath;
 }
 
@@ -129,6 +165,11 @@ function closeSession()
     router.push('/sports2');
 }
 
+function toggleAdvice()
+{
+    showAdvice.value = !showAdvice.value;
+}
+
 function finishStretches()
 {
     router.push(
@@ -137,6 +178,125 @@ function finishStretches()
 }
 </script>
 
-
 <style lang="less" scoped>
+#stretch-container-2 {
+    background-image: url('@/assets2/images/bg-20.png');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+
+    .stretch2-content {
+        background-image: url('@/assets2/images/skate-cover.png');
+        &::before {
+            content: '';
+            z-index: 1;
+            background-color: @black;
+            opacity: 0.33;
+            position: absolute;
+            width: 100%;
+            height: 60%;
+            top: 0;
+            left: 0;
+        }
+
+        background-size: cover;
+        box-sizing: border-box;
+        background-position: center;
+        background-repeat: no-repeat;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        height: 60%;
+        width: 100%;
+        z-index: 0;
+        padding: 1.5rem 1rem;
+
+        .stretch2-header {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            z-index: 2;
+            text-align: left;
+
+            .stretch2-title {
+                font-size: @h2;
+                font-weight: @medium;
+                color: @white;
+            }
+            .stretch2-indication {
+                width: 100%;
+                font-size: @h3;
+                font-weight: @regular;
+                color: @white;
+                opacity: 0.75;
+            }
+        }
+
+        .stretch2-img {
+            width: 100%;
+            height: 100%;
+            z-index: 2;
+        }
+        .stretch2-toolbar {
+            display: flex;
+            justify-content: space-between;
+            width: 100%;
+            z-index: 2;
+
+            .stretch2-icon {
+                width: 2.5rem;
+                height: 2.5rem;
+            }
+        }
+    }
+
+    .stretch2-infos {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: start;
+        gap: 1rem;
+        padding: 1.5rem 1rem;
+        z-index: 2;
+        height: 100%;
+
+        .stretch2-description {
+            font-size: @h3;
+            font-weight: @light;
+            color: @black;
+            text-align: left;
+            width: 100%;
+        }
+
+        .stretch2-hint {
+            display: flex;
+            align-items: center;
+            justify-content: start;
+            gap: 16px;
+            width: 100%;
+
+            .stretch2-icon {
+                width: 1.5rem;
+                height: 1.5rem;
+            }
+            .stretch2-text {
+                line-height: 0.8;
+                font-size: @h3;
+                font-weight: @thin;
+                color: @black;
+            }
+        }
+    }
+
+    .stretch2-navbar {
+        display: flex;
+        width: 100%;
+        box-sizing: border-box;
+        justify-content: space-between;
+
+        padding: 1.5rem 1rem;
+    }
+}
 </style>
